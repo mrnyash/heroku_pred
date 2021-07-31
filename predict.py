29911c42ad57af9c72510b9f975372b05d24874f
Yash Prajapati <prajapatiyash3@gmail.com>
2021-07-31 18:31:42 +0530

    deployment

---
+++ predict.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import numpy as np
+from flask.globals import request
+import joblib
+app = Flask(__name__) # Behind the scene it is converted to main.py
+
+
+# load a model
+model = joblib.load('hiring_model.pkl')
+
+@app.route('/')
+def hello_world():
+    return render_template('base.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+    exp = request.form.get('experience')
+    score = request.form.get('test_score')
+    interview_score = request.form.get('Interview_score')
+
+    prediciton = model.predict([[exp, score, interview_score]])
+
+    output = round(prediciton[0], 2)
+ 
+ 
+
+    return render_template('base.html', prediction_text = f"Employee Salary will be $ {output}")
+
+
+# @app.route('/welcome')
+# def welcome():
+#     return 'Welcome my Friend'
+
+app.run(debug=True) # debug = true will reload the server
